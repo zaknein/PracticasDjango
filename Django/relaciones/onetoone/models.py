99177eac80f76fbda899d44a25f6314b7b0780ad
German Kröger <gkroger11@gmail.com>
2023-06-05 18:41:05 -0300

    modelos, migraciones y relacion uno a uno ready

--- Django/relaciones/onetoone/models.py
+++ Django/relaciones/onetoone/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Lugar(models.Model):
     name = models.CharField(max_length=50)
-    andress = models.CharField(max_length=80)
+    adress = models.CharField(max_length=80)
 
     def __str__(self):
         return self.name
 
 
 class Restaurante(models.Model):
-    lugar = models.OneToOneField(Lugar, on_delete = CASCADE, primary_key=True)
+    lugar = models.OneToOneField(Lugar, on_delete = models.CASCADE, primary_key=True)
     n_empleados = models.IntegerField(default=1)   
 
     def __str__(self):
--- Django/relaciones/onetoone/views.py
+++ Django/relaciones/onetoone/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lugar, Restaurante
 
-# Create your views here.
+def crear(request):
+    #  TODO Crear elemento
+    lugar = Lugar(name='place1', adress = 'Calle falsa 123')
+    lugar.save()
+    
+    restaurante = Restaurante(lugar=lugar, n_empleados = 10 )
+    restaurante.save()
+
+    return HttpResponse(restaurante.lugar.name)
+

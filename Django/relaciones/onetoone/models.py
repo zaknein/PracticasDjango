87dbd38a11280a45f2b2e137b1f38c904f684859
German Kröger <gkroger11@gmail.com>
2023-06-05 17:47:14 -0300

    creacion de la relacion uno a uno modelos

--- Django/relaciones/onetoone/models.py
+++ Django/relaciones/onetoone/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Lugar(models.Model):
+    name = models.CharField(max_length=50)
+    andress = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurante(models.Model):
+    lugar = models.OneToOneField(Lugar, on_delete = CASCADE, primary_key=True)
+    n_empleados = models.IntegerField(default=1)   
+
+    def __str__(self):
+        return self.place.name

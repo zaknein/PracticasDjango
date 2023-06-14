1e08ad01f82136ede0ba77b456782cb76258c71c
German Kröger <gkroger11@gmail.com>
2023-06-14 20:15:57 -0300

    migrations

--- Django/relaciones/muchosauno/models.py
+++ Django/relaciones/muchosauno/models.py
@@ -9,4 +9,14 @@ class Reportero(models.Model):
     def __str__(self):
         return self.email
 
-        
+# para hacer una vinculacion muchos a uno,
+# el elemento que solo tiene uno del otro es el que tiene la clave foranea
+# como un articulo solo puede tener un reportero, es el que tiene que tener la relacion
+
+class Articulo(models.Model):
+    titular = models.CharField(max_length=30)
+    fecha = models.DateField()
+    reportero = models.ForeignKey(Reportero, on_delete=models.CASCADE)
+
+    def __str__(self):
+       return self.titular     
--- Django/relaciones/muchosauno/views.py
+++ Django/relaciones/muchosauno/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def create(req):
+def crear(req):
     return HttpResponse('Funciono')

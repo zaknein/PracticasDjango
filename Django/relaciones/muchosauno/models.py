36b369433251bd4dbbc5eedef48fc620221a82de
German Kröger <gkroger11@gmail.com>
2023-06-14 20:11:10 -0300

    app muchosauno parte 1

---
+++ Django/relaciones/muchosauno/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Reportero(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
+        
--- Django/relaciones/relaciones/urls.py
+++ Django/relaciones/relaciones/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('one/', include('onetoone.urls'))
+    path('one/', include('onetoone.urls')),
+    path('muchos', include('muchosauno.urls'))
 ]

241fc0b7dd81db3230a5edc296cd3c7882f872b4
German Kröger <gkroger11@gmail.com>
2023-06-15 17:54:03 -0300

    terminado de relacion many to one

--- Django/relaciones/muchosauno/views.py
+++ Django/relaciones/muchosauno/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Reportero, Articulo
+from datetime import date
+
 
 
 def crear(req):
-    return HttpResponse('Funciono')
+    rep = Reportero(nombre= 'pablo', apellido='menz', email='menz@mail.com')
+    rep.save()
+    art= Articulo(titular='lorem impsum',fecha=date(2022,5,5),reportero=rep)
+    art.save()
+    art2= Articulo(titular='lre impsum',fecha=date(2023,6,5),reportero=rep)
+    art2.save()
+    art3= Articulo(titular='los impsum',fecha=date(2021,7,5),reportero=rep)
+    art3.save()
+
+    # result = art.reportero.nombre
+    result = rep.articulo_set.all()
+    return HttpResponse(result)
--- Django/relaciones/relaciones/urls.py
+++ Django/relaciones/relaciones/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('one/', include('onetoone.urls')),
-    path('muchos', include('muchosauno.urls'))
+    path('muchos/', include('muchosauno.urls'))
 ]

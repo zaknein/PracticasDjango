d65f3ceb00908e7c61177a1213a428acaf41436b
German Kröger <gkroger11@gmail.com>
2023-06-16 17:36:44 -0300

    datos modelados y relacionasdos

--- Django/relaciones/muchosamuchos/models.py
+++ Django/relaciones/muchosamuchos/models.py
@@ -6,11 +6,18 @@ class Publicacion(models.Model):
     def __str__(self):
         return self.titulo
 
+# para hacer una relacion muchos a muchos
+# hay que declararlo en una de las dos clases implicadas,
+# no hace falta en ambas
 
 class Articulo(models.Model):
 
     titular = models.CharField(max_length=100)
+    publicaciones = models.ManyToManyField(Publicacion)
+    # solo necesita el modelo,
+    # no es necesario un metodo de borrado, 
+    # puesto que se crea una tercer table
 
 
     def __str__(self):
-        return self.titular   
+        return self.titular

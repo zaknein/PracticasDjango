f202d3835599196d3bf3030fcb05c3d4f9845b16
German Kröger <gkroger11@gmail.com>
2023-06-16 17:33:41 -0300

    creacion de modelos y clases sin relacionar

--- Django/relaciones/muchosamuchos/models.py
+++ Django/relaciones/muchosamuchos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Articulo(models.Model):
+
+    titular = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.titular   

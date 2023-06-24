b1990f8331bda322b3b30e10232bad9e4a2782d6
German Kröger <gkroger11@gmail.com>
2023-06-24 13:01:59 -0300

    migration initial

--- Django/Proyectomodelosyvistas/Empleados/models.py
+++ Django/Proyectomodelosyvistas/Empleados/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
+    
+class Salary(models.Model):
+    cantidad = models.IntegerField()
+    aguinaldo_jun = models.BooleanField()
+    aguinaldo_dic = models.BooleanField()
+
+class Job(models.Model):
+    titulo = models.CharField(max_length=20, null=False)
+    descripcion = models.TextField(max_length= 200)
+    salario = models.ForeignKey(Salary, on_delete=models.CASCADE)
+
+class Country(models.Model):
+    nombre = models.CharField(max_length=20,null=False)
+    country_code = models.IntegerField(max_length=3, null = False)
+    
+class Location(models.Model):
+    nombre = models.CharField(max_length=30, null=False)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    
+class Place(models.Model):
+    nombre = models.CharField(max_length=30, null=False)
+    direccion = models.CharField(max_length=30, null= False)
+    codigo_postal = models.IntegerField(max_length=10)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    
+
 class Empleado(models.Model):
     nombre = models.CharField(max_length= 20, null=False)
     apellido = models.CharField(max_length=20, null=False)
@@ -10,27 +36,3 @@ class Empleado(models.Model):
 
     def __str__(self):
         return self.nombre
-
-class Job(models.Model):
-    titulo = models.CharField(max_length=20, null=False)
-    descripcion = models.TextField(max_length= 200)
-    salario = models.ForeignKey(Salary, on_delete=models.CASCADE)
-    
-class Salary(models.Model):
-    cantidad = models.IntegerField()
-    aguinaldo_jun = models.BooleanField()
-    aguinaldo_dic = models.BooleanField()
-
-class Place(models.Model):
-    nombre = models.CharField(max_length=30, null=False)
-    direccion = models.CharField(max_length=30, null= False)
-    codigo_postal = models.IntegerField(max_length=10)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-
-class Location(models.Model):
-    nombre = models.CharField(max_length=30, null=False)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    
-class Country(models.Model):
-    nombre = models.CharField(max_length=20,null=False)
-    country_code = models.IntegerField(max_length=3, null = False)

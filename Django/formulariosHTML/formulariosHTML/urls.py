24c72e2cf233635e8d956d7028e0aa4c20e790da
German Kröger <gkroger11@gmail.com>
2023-07-05 17:13:34 -0300

    formularios post mas csrf

--- Django/formulariosHTML/formulariosHTML/urls.py
+++ Django/formulariosHTML/formulariosHTML/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('form/', views.form , name='form'),
-    path('goal/', views.goal, name ='goal'),
+    path('get/form/', views.getform , name='form'),
+    path('get/goal/', views.getgoal, name ='goal'),
+    path('post/form', views.postform, name = 'postform'),
+    path('post/goal', views.postgoal, name = 'postgoal')
+
 ]
--- Django/formulariosHTML/formulariosHTML/views.py
+++ Django/formulariosHTML/formulariosHTML/views.py
@@ -5,12 +5,22 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html', {})
     
-def form(request):
+def getform(request):
     return render(request, 'form.html', {})
 
-def goal(request):
+def getgoal(request):
     if request.method != 'GET':
         return HttpResponse('You donkey piece of shit')
 
     name = request.GET['name']
     return render(request, 'succes.html', {'name': name})
+
+def postform(request):
+    return render(request, 'postform.html', {})
+
+def postgoal(request):
+    if request.method != 'POST':
+        return HttpResponse('El metodo GET no es soportado por este formulario')
+    
+    info = request.POST['info']
+    return render(request, 'postsucces.html', { 'info':info })

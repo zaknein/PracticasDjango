2f6d77dd8972efac44052fe8561d068ce6e2920f
German Kröger <gkroger11@gmail.com>
2023-07-12 17:09:40 -0300

    forms finish?

--- Django/ecommerce/stock/models.py
+++ Django/ecommerce/stock/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50,blank=False,null=False)
+    short_descripcion = models.CharField(max_length=100,blank=False,null=False)
+    descripcion = models.TextField(blank=False, null=False)
+    stock = models.IntegerField(default=20)
+
+    def __str__(self):
+        return self.name
--- Django/ecommerce/stock/views.py
+++ Django/ecommerce/stock/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
-
+from .forms import ProductForm
+ 
 def index(request):
-    return render (request,'index.html',{})
+    if request.method == 'POST':
+        # Guardar la informacion
+        form1 = ProductForm(request.POST)
+        if form1.is_valid():
+            form1.save()
+
+  
+    form = ProductForm()
+        
+    return render (request,'index.html',{'form': form})
